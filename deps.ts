ade16b8f13b6b0e932810859b1e49f99d4097c92
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-27 20:01:31 +0900

    docs(deps): add code comment

--- deps.ts
+++ deps.ts
@@ -5,6 +5,7 @@ export { isString } from "https://deno.land/x/isx@1.1.0/is_string.ts";
 
 // TODO:(miyauci) Packaging and externalization this module.
 
+/** Types for uppercase letter. */
 export type UppercaseLetter =
   | "A"
   | "B"
@@ -32,18 +33,28 @@ export type UppercaseLetter =
   | "X"
   | "Y"
   | "Z";
+
+/** Types for lowercase letter. */
 export type LowercaseLetter = Lowercase<UppercaseLetter>;
 
+/** Types for letter. */
 export type Letter = UppercaseLetter | LowercaseLetter;
 
+/** Types for digit. */
 export type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
+/** Deserialize types. */
 export type Stringify<
   T extends string | number | bigint | boolean | null | undefined,
 > = `${T}`;
 
+/** Representation of [`<DIGIT>`](https://www.rfc-editor.org/rfc/rfc5234#appendix-B.1). */
 export type DIGIT = Stringify<Digit>;
+
+/** Representation of [`<ALPHA>`](https://www.rfc-editor.org/rfc/rfc5234#appendix-B.1). */
 export type ALPHA = Letter;
+
+/** Representation of [`<tchar>`](https://www.rfc-editor.org/rfc/rfc9110#section-5.6.2-2). */
 export type TChar =
   | "!"
   | "#"
